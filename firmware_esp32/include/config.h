5a2dd9e5522a8d86bf78c3b46f20af581ee91ade
Lucca Aguilar <sophia.santos.aguilar@gmail.com>
2026-01-28 14:59:54 -0300

    Adiciona código de leitura de encoders.

--- firmware_esp32/include/config.h
+++ firmware_esp32/include/config.h
@@ -1,6 +1,30 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
+// --- LADO DIREITO (Motores 1 e 3) ---
+#define M1_IN1      26
+#define M1_IN2      27
+#define M1_PWM      14
+#define M1_ENC_A    17
+#define M1_ENC_B    16
 
+#define M3_IN1      32
+#define M3_IN2      33
+#define M3_PWM      25
+#define M3_ENC_A    35
+#define M3_ENC_B    34
+
+// --- LADO ESQUERDO (Motores 2 e 4) ---
+#define M2_IN1      2
+#define M2_IN2      15
+#define M2_PWM      4 
+#define M2_ENC_A    23
+#define M2_ENC_B    22
+
+#define M4_IN1      18
+#define M4_IN2      19
+#define M4_PWM      21
+#define M4_ENC_A    39
+#define M4_ENC_B    36
 
 #endif // CONFIG_H
--- firmware_esp32/src/main.cpp
+++ firmware_esp32/src/main.cpp
@@ -1,74 +1,101 @@
 #include <Arduino.h>
-#include <micro_ros_platformio.h>
+#include "config.h" 
 
-#include <rcl/rcl.h>
-#include <rclc/rclc.h>
-#include <rclc/executor.h>
+#define PINO_MOTOR_IN1      M3_IN1  
+#define PINO_MOTOR_IN2      M3_IN2  
+#define PINO_ENABLE_PWM     M3_PWM 
 
-#include <std_msgs/msg/int32.h>
+// Configurações do Encoder
+#define PULSOS_POR_VOLTA    1440  
+#define INTERVALO_LEITURA   100   // em milissegundos
 
-#define LED_PIN 2           
-#define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){error_loop();}}
-#define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){}}
+// Configurações do PWM (LEDC)
+#define FREQ_PWM            5000 
+#define CANAL_PWM           0    
+#define RESOLUCAO_PWM       8  
 
-rcl_publisher_t publisher;
-std_msgs__msg__Int32 msg;
-rclc_executor_t executor;
-rclc_support_t support;
-rcl_allocator_t allocator;
-rcl_node_t node;
-rcl_timer_t timer;
+// Variáveis para o Encoder (Volatile é obrigatório para interrupções)
+volatile long ticks_contador = 0; 
 
-void error_loop(){
-  while(1){
-    digitalWrite(LED_PIN, !digitalRead(LED_PIN));
-    delay(100);
-  }
-}
+// Variáveis para cálculo de RPM
+unsigned long tempo_anterior = 0;
+long ticks_anterior = 0;
 
-void timer_callback(rcl_timer_t * timer, int64_t last_call_time)
-{
-  RCLC_UNUSED(last_call_time);
-  if (timer != NULL) {
-    RCSOFTCHECK(rcl_publish(&publisher, &msg, NULL));
-    msg.data++;
+bool sentido_horario = false; 
+
+// --- FUNÇÃO DE INTERRUPÇÃO (ISR) ---
+// Roda automaticamente toda vez que o encoder A mudar de LOW para HIGH
+void IRAM_ATTR lerEncoder() {
+  // Lê o estado do Canal B para saber a direção real
+  int estado_b = digitalRead(M3_ENC_B);
+  
+  // Se B for diferente de A (ou HIGH na borda de subida), incrementa ou decrementa
+  if (estado_b > 0) {
+    ticks_contador++;
+  } else {
+    ticks_contador--;
   }
 }
 
 void setup() {
   Serial.begin(115200);
-  set_microros_serial_transports(Serial);
 
-  pinMode(LED_PIN, OUTPUT);
-  digitalWrite(LED_PIN, HIGH);
-  delay(2000);
+  // Configuração Motor
+  pinMode(PINO_MOTOR_IN1, OUTPUT);
+  pinMode(PINO_MOTOR_IN2, OUTPUT);
+  
+  // Configuração PWM
+  ledcSetup(CANAL_PWM, FREQ_PWM, RESOLUCAO_PWM);
+  ledcAttachPin(PINO_ENABLE_PWM, CANAL_PWM);
 
-  allocator = rcl_get_default_allocator();
+  // Configuração Encoder
+  pinMode(M3_ENC_A, INPUT); 
+  pinMode(M3_ENC_B, INPUT);
 
-  RCCHECK(rclc_support_init(&support, 0, NULL, &allocator));
-
-  RCCHECK(rclc_node_init_default(&node, "esp32_node", "", &support));
-
-  RCCHECK(rclc_publisher_init_default(
-    &publisher,
-    &node,
-    ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
-    "micro_ros_counter"));
-
-  const unsigned int timer_timeout = 1000;
-  RCCHECK(rclc_timer_init_default(
-    &timer,
-    &support,
-    RCL_MS_TO_NS(timer_timeout),
-    timer_callback));
-
-  RCCHECK(rclc_executor_init(&executor, &support.context, 1, &allocator));
-  RCCHECK(rclc_executor_add_timer(&executor, &timer));
-
-  msg.data = 0;
+  // Ativa a interrupção no pino A (chama a função 'lerEncoder' na subida do sinal)
+  attachInterrupt(digitalPinToInterrupt(M3_ENC_A), lerEncoder, RISING);
 }
 
 void loop() {
-  delay(100);
-  RCCHECK(rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100)));
+  // --- CONTROLE DO MOTOR (Mantido do seu código) ---
+  if (sentido_horario) {
+    digitalWrite(PINO_MOTOR_IN1, HIGH);
+    digitalWrite(PINO_MOTOR_IN2, LOW);
+  } else {
+    digitalWrite(PINO_MOTOR_IN1, LOW);
+    digitalWrite(PINO_MOTOR_IN2, HIGH);
+  }
+
+  // Velocidade fixa para teste
+  ledcWrite(CANAL_PWM, 240);
+  
+  // --- CÁLCULO DE RPM E IMPRESSÃO (A cada 100ms) ---
+  unsigned long tempo_atual = millis();
+  
+  if (tempo_atual - tempo_anterior >= INTERVALO_LEITURA) {
+    
+    // 1. Captura segura dos ticks (pausa interrupção rapidinho para ler variável volátil)
+    noInterrupts();
+    long ticks_agora = ticks_contador;
+    interrupts();
+
+    // 2. Calcula quantos ticks passaram desde a última leitura
+    long delta_ticks = ticks_agora - ticks_anterior;
+
+    // 3. Fórmula do RPM:
+    // (Ticks_no_Intervalo / Ticks_por_Volta) * (Ms_em_um_Minuto / Intervalo_Leitura)
+    float rpm = ((float)delta_ticks / PULSOS_POR_VOLTA) * (60000.0 / INTERVALO_LEITURA);
+
+    // 4. Mostra no Serial Plotter/Monitor
+    // Dica: Use o Serial Plotter do VSCode para ver o gráfico!
+    Serial.print(">Ticks: ");
+    Serial.print(ticks_agora);
+    Serial.print(" | ");
+    Serial.print(">RPM: ");
+    Serial.println(rpm);
+
+    // 5. Atualiza as variáveis de controle
+    ticks_anterior = ticks_agora;
+    tempo_anterior = tempo_atual;
+  }
 }
