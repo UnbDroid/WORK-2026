a04775d51b584ca0f15922c0bf11743f1bcc6122
Isabela Furlan <isabelasfurlann@gmail.com>
2026-05-22 14:30:49 -0300

    Odometria-rf2o

--- work_ws/src/salamander_bot/launch/mapping.launch.py
+++ work_ws/src/salamander_bot/launch/mapping.launch.py
@@ -4,36 +4,25 @@ from ament_index_python.packages import get_package_share_directory
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch import LaunchDescription
-from launch_ros.actions import Node
-
-import xacro
 
 ''' Sobe:
-        robot_state_publisher
-        odom_node
-        ekf_node
+        odomLidar
         slam_toolbox
 '''
 
 def generate_launch_description():
     salamander_share = get_package_share_directory('salamander_bot')
 
-    robot_description_path = os.path.join(
-        salamander_share,
-        'description',
-        'robot.urdf.xacro'
-    )
-
-    ekf_config_path = os.path.join(
-        salamander_share,
-        'config',
-        'ekf.yaml'
-    )
-
     slam_config_path = os.path.join(
         salamander_share,
         'config',
         'mapper_params_online_async.yaml'
+    )
+
+    odom_lidar_launch_path = os.path.join(
+        salamander_share,
+        'launch',
+        'odomLidar.launch.py'
     )
 
     slam_toolbox_share = get_package_share_directory('slam_toolbox')
@@ -43,31 +32,9 @@ def generate_launch_description():
         'online_async_launch.py'
     )
 
-    robot_description = xacro.process_file(robot_description_path).toxml()
-
     return LaunchDescription([
-        Node(
-            package='robot_state_publisher',
-            executable='robot_state_publisher',
-            parameters=[
-                {'robot_description': robot_description}
-            ],
-            output='screen'
-        ),
-
-        Node(
-            package='robot_odometry',
-            executable='odom_node',
-            name='odom_node',
-            output='screen'
-        ),
-
-        Node(
-            package='robot_localization',
-            executable='ekf_node',
-            name='ekf_filter_node',
-            parameters=[ekf_config_path],
-            output='screen'
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(odom_lidar_launch_path)
         ),
 
         IncludeLaunchDescription(
