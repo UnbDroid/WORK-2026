2d0e68ff479238b07dfb3c3a366306a4a645ede4
enzo-camara <242034625@aluno.unb.br>
2026-06-01 11:11:58 -0300

    Navegação Atualizada

--- work_ws/src/salamander_bot/launch/mapping.launch.py
+++ work_ws/src/salamander_bot/launch/mapping.launch.py
@@ -1,17 +1,16 @@
 import os
 
 from ament_index_python.packages import get_package_share_directory
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
 from launch import LaunchDescription
-
-''' Sobe:
-        odomLidar
-        slam_toolbox
-'''
 
 def generate_launch_description():
     salamander_share = get_package_share_directory('salamander_bot')
+
+    # DECLARADO: Argumento para controlar o tempo de simulação de forma dinâmica
+    use_sim_time = LaunchConfiguration('use_sim_time', default='false')
 
     slam_config_path = os.path.join(
         salamander_share,
@@ -33,6 +32,13 @@ def generate_launch_description():
     )
 
     return LaunchDescription([
+        # Declaração do argumento para o sistema
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value='false',
+            description='Use simulation (Gazebo) clock if true'
+        ),
+
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource(odom_lidar_launch_path)
         ),
@@ -41,7 +47,7 @@ def generate_launch_description():
             PythonLaunchDescriptionSource(slam_toolbox_launch_path),
             launch_arguments={
                 'slam_params_file': slam_config_path,
-                'use_sim_time': 'false',
+                'use_sim_time': use_sim_time, # MODIFICADO: Agora aceita dinamicamente true ou false
             }.items()
         )
     ])
