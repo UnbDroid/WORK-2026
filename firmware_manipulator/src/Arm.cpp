3134c3f3b10de8515221b44b1438b81041bcd059
Daniel Abreu <daniel.frioli@gmail.com>
2026-06-25 23:58:17 -0300

    Added Simple Forward Kinematics to drive the Manipulator in radians, solve arm not responding if autoEnable(false) and tweaks in vel/acel values

--- firmware_manipulator/src/Arm.cpp
+++ firmware_manipulator/src/Arm.cpp
@@ -5,6 +5,19 @@
 #include "Config.h"
 #include "Arm.h"
 
+constexpr double BASE_STEPS_PER_RAD = (STEPS_PER_REV_BASE * MICROSTEPPING_MULT) / (2.0 * PI);
+
+constexpr double ARM_STEPS_PER_RAD = (STEPS_PER_REV_ARM * MICROSTEPPING_MULT) / (2.0 * PI);
+
+inline int32_t baseRadToSteps(double rad)
+{
+    return lround(rad * BASE_STEPS_PER_RAD);
+}
+
+inline int32_t armRadToSteps(double rad)
+{
+    return lround(rad * ARM_STEPS_PER_RAD);
+}
 
 Manipulator::Manipulator() {
     this->stepper_base = nullptr;
@@ -30,13 +43,13 @@ void Manipulator::drive_angle(double theta_base, double theta_arm, double theta_
     this->theta_gripper = theta_gripper;
     
     if (stepper_base) {
-        stepper_base->moveTo(theta_base);
+        stepper_base->moveTo(baseRadToSteps(theta_base));
     };
     if (stepper_arm) {
-        stepper_arm->moveTo(theta_arm);
+        stepper_arm->moveTo(armRadToSteps(theta_arm));
     };
     if (servo_gripper) {
-        servo_gripper->write(theta_gripper);
+        servo_gripper->write(theta_gripper * 180.0 / PI);
     };
 };
 
--- firmware_manipulator/src/main.cpp
+++ firmware_manipulator/src/main.cpp
@@ -17,15 +17,18 @@ void setup() {
   pinMode(MS1_M1_PIN, OUTPUT);
   pinMode(MS2_M1_PIN, OUTPUT);
   pinMode(MS3_M1_PIN, OUTPUT);
+
   pinMode(MS1_M2_PIN, OUTPUT);
   pinMode(MS2_M2_PIN, OUTPUT);
   pinMode(MS3_M2_PIN, OUTPUT);
+
   pinMode(ENABLE_M1_PIN, OUTPUT);
   pinMode(ENABLE_M2_PIN, OUTPUT);
 
   digitalWrite(MS1_M1_PIN, HIGH);
   digitalWrite(MS2_M1_PIN, HIGH);
   digitalWrite(MS3_M1_PIN, HIGH);
+
   digitalWrite(MS1_M2_PIN, HIGH);
   digitalWrite(MS2_M2_PIN, HIGH);
   digitalWrite(MS3_M2_PIN, HIGH);
@@ -43,21 +46,23 @@ void setup() {
     stepper_base->setAutoEnable(true);
 
     stepper_base->setSpeedInHz((1.0f * STEPS_PER_REV_BASE * MICROSTEPPING_MULT) / (2.0f * PI));
-    stepper_base->setAcceleration((1.5f * STEPS_PER_REV_BASE * MICROSTEPPING_MULT) / (2.0f * PI));
+    stepper_base->setAcceleration((0.4f * STEPS_PER_REV_BASE * MICROSTEPPING_MULT) / (2.0f * PI));
   }
 
   if (stepper_arm) {
     stepper_arm->setDirectionPin(DIR_M2_PIN);
     stepper_arm->setEnablePin(ENABLE_M2_PIN);
-    stepper_arm->setAutoEnable(true);
+    stepper_arm->setAutoEnable(false);
 
-    stepper_arm->setSpeedInHz((1.0f * STEPS_PER_REV_ARM * MICROSTEPPING_MULT) / (2.0f * PI));
-    stepper_arm->setAcceleration((1.5f * STEPS_PER_REV_ARM * MICROSTEPPING_MULT) / (2.0f * PI));
+    stepper_arm->enableOutputs();
+
+    stepper_arm->setSpeedInHz((0.6f * STEPS_PER_REV_ARM * MICROSTEPPING_MULT) / (2.0f * PI));
+    stepper_arm->setAcceleration((0.3f * STEPS_PER_REV_ARM * MICROSTEPPING_MULT) / (2.0f * PI));
   }
   
   Arm.init(stepper_base, stepper_arm, &gripper);
 
-  Arm.drive_angle(22400, 7400, 110);
+  Arm.drive_angle(PI, PI/6, PI);
 }
 
 void loop() {
