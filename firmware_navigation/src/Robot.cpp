af337bae59000bac38f460418e84a25890f973df
enzo-camara <242034625@aluno.unb.br>
2026-08-03 14:00:04 -0300

    robot.cpp alterado

--- firmware_navigation/src/Robot.cpp
+++ firmware_navigation/src/Robot.cpp
@@ -3,6 +3,8 @@
 #include "MotorDC.h"
 #include "Robot.h"
 
+constexpr double YAW_PARAMETER = (DISTANCE_BETWEEN_WHEELS_X + DISTANCE_BETWEEN_WHEELS_Y);
+
 Robot::Robot(MotorDC& Motor1, MotorDC& Motor2, MotorDC& Motor3, MotorDC& Motor4) : Motor1(Motor1), Motor2(Motor2), Motor3(Motor3), Motor4(Motor4){
     this->Vx = 0.0;
     this->Vy = 0.0;
@@ -10,17 +12,13 @@ Robot::Robot(MotorDC& Motor1, MotorDC& Motor2, MotorDC& Motor3, MotorDC& Motor4)
 }
 
 void Robot::moveRobot(double Vx, double Vy, double Omega) {
-    // Conversão: rad/s -> m/s:
-    double Vx_meters = Vx * WHEEL_RADIUS;
-    double Vy_meters = Vy * WHEEL_RADIUS;
+    // Cálculo de Cinemática Inversa:
+    double V_M1 = Vx + Vy + YAW_PARAMETER * Omega;
+    double V_M2 = Vx - Vy - YAW_PARAMETER * Omega;
+    double V_M3 = Vx - Vy + YAW_PARAMETER * Omega;
+    double V_M4 = Vx + Vy - YAW_PARAMETER * Omega;
 
-    // Cálculo da Cinemática Inversa:
-    double V_M1 = Vx_meters + Vy_meters + (DISTANCE_BETWEEN_WHEELS_Y * Omega) - (DISTANCE_BETWEEN_WHEELS_X * Omega);
-    double V_M2 = Vx_meters - Vy_meters - (DISTANCE_BETWEEN_WHEELS_Y * Omega) + (DISTANCE_BETWEEN_WHEELS_X * Omega);
-    double V_M3 = Vx_meters - Vy_meters + (DISTANCE_BETWEEN_WHEELS_Y * Omega) - (DISTANCE_BETWEEN_WHEELS_X * Omega);
-    double V_M4 = Vx_meters + Vy_meters - (DISTANCE_BETWEEN_WHEELS_Y * Omega) + (DISTANCE_BETWEEN_WHEELS_X * Omega);
-
-    // Conversão: m/s -> RPM:
+    // Conversão: Metros por Segundo => RPM:
     double V_M1_RPM = (V_M1 * 60.f) / (PI * 2.f * WHEEL_RADIUS);
     double V_M2_RPM = (V_M2 * 60.f) / (PI * 2.f * WHEEL_RADIUS);
     double V_M3_RPM = (V_M3 * 60.f) / (PI * 2.f * WHEEL_RADIUS);
