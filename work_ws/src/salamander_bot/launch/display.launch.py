24099aac6e552e3892b54786f7872f6f72e93f63
Daniel Abreu <daniel.frioli@gmail.com>
2026-08-04 18:41:45 -0300

    Mudança de Arquivo de Configuração

--- work_ws/src/salamander_bot/launch/display.launch.py
+++ work_ws/src/salamander_bot/launch/display.launch.py
@@ -19,7 +19,7 @@ def generate_launch_description():
     robot_description_config = xacro.process_file(xacro_file)
     robot_urdf = robot_description_config.toxml()
 
-    rviz_config_file = os.path.join(share_dir, 'config', 'display.rviz')
+    rviz_config_file = os.path.join(share_dir, 'config', 'caldo.rviz')
 
     gui_arg = DeclareLaunchArgument(
         name='gui',
