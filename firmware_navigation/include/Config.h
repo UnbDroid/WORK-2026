ccb2941be2cfc3cd6f9534b0001237cd0bfeafea
enzo-camara <242034625@aluno.unb.br>
2026-09-14 13:37:08 -0300

    Pinos dos motores organizados e main.cpp descomentado

--- firmware_navigation/include/Config.h
+++ firmware_navigation/include/Config.h
@@ -15,8 +15,8 @@
 #define DISTANCE_BETWEEN_WHEELS_Y   0.14875   // Distance Between Wheels and Center of Robot (Y Axes) in Meters.
 
 
-#define M1_IN1          GPIO_NUM_26           // IN1 Pin Connected to Motor Driver.
-#define M1_IN2          GPIO_NUM_27           // IN2 Pin Connected to Motor Driver.
+#define M1_ENA          GPIO_NUM_26           // ENA Pin Connected to Motor Driver.
+#define M1_ENB          GPIO_NUM_27           // ENB Pin Connected to Motor Driver.
 #define M1_PWM          GPIO_NUM_13           // Enable Pin for PWM Control.
 #define M1_ENC_A        GPIO_NUM_16           // Encoder A (Yellow Wire).
 #define M1_ENC_B        GPIO_NUM_17           // Encoder B Pin (White Wire).
@@ -24,8 +24,8 @@
 #define M1_KP           1.5
 #define M1_KI           0.75
 
-#define M3_IN1          GPIO_NUM_32
-#define M3_IN2          GPIO_NUM_33
+#define M3_ENA          GPIO_NUM_33           // ENA
+#define M3_ENB          GPIO_NUM_32           // ENB
 #define M3_PWM          GPIO_NUM_25
 #define M3_ENC_A        GPIO_NUM_35
 #define M3_ENC_B        GPIO_NUM_34         
@@ -33,8 +33,8 @@
 #define M3_KP           1.5
 #define M3_KI           0.75
 
-#define M2_IN1          GPIO_NUM_22
-#define M2_IN2          GPIO_NUM_23
+#define M2_ENA          GPIO_NUM_23
+#define M2_ENB          GPIO_NUM_22
 #define M2_PWM          GPIO_NUM_4
 #define M2_ENC_A        GPIO_NUM_14
 #define M2_ENC_B        GPIO_NUM_15 
@@ -42,11 +42,11 @@
 #define M2_KP           1.75
 #define M2_KI           0.75
 
-#define M4_IN1          GPIO_NUM_19
-#define M4_IN2          GPIO_NUM_18
+#define M4_ENA          GPIO_NUM_19
+#define M4_ENB          GPIO_NUM_18
 #define M4_PWM          GPIO_NUM_21
-#define M4_ENC_A        GPIO_NUM_39
-#define M4_ENC_B        GPIO_NUM_36
+#define M4_ENC_A        GPIO_NUM_36
+#define M4_ENC_B        GPIO_NUM_39
 #define M4_CANAL_PWM    LEDC_CHANNEL_3
 #define M4_KP           1.5
 #define M4_KI           0.75
