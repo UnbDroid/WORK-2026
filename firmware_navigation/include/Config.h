a8d896cf30279ed2a7f2e7778187a450052c0b30
enzo-camara <242034625@aluno.unb.br>
2026-09-17 15:43:06 -0300

    Pinos dos motores agora estao certos

--- firmware_navigation/include/Config.h
+++ firmware_navigation/include/Config.h
@@ -24,8 +24,8 @@
 #define M1_KP           1.5
 #define M1_KI           0.75
 
-#define M3_ENA          GPIO_NUM_33           // ENA
-#define M3_ENB          GPIO_NUM_32           // ENB
+#define M3_ENA          GPIO_NUM_32           // ENA
+#define M3_ENB          GPIO_NUM_33           // ENB
 #define M3_PWM          GPIO_NUM_25
 #define M3_ENC_A        GPIO_NUM_35
 #define M3_ENC_B        GPIO_NUM_34         
@@ -33,8 +33,8 @@
 #define M3_KP           1.5
 #define M3_KI           0.75
 
-#define M2_ENA          GPIO_NUM_23
-#define M2_ENB          GPIO_NUM_22
+#define M2_ENA          GPIO_NUM_22
+#define M2_ENB          GPIO_NUM_23
 #define M2_PWM          GPIO_NUM_4
 #define M2_ENC_A        GPIO_NUM_14
 #define M2_ENC_B        GPIO_NUM_15 
@@ -45,8 +45,8 @@
 #define M4_ENA          GPIO_NUM_19
 #define M4_ENB          GPIO_NUM_18
 #define M4_PWM          GPIO_NUM_21
-#define M4_ENC_A        GPIO_NUM_36
-#define M4_ENC_B        GPIO_NUM_39
+#define M4_ENC_A        GPIO_NUM_39
+#define M4_ENC_B        GPIO_NUM_36
 #define M4_CANAL_PWM    LEDC_CHANNEL_3
 #define M4_KP           1.5
 #define M4_KI           0.75
