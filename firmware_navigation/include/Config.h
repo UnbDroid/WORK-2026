0a6c14e0b7a9afc59a7983523ee70ce31511b07d
enzo-camara <242034625@aluno.unb.br>
2026-09-22 15:07:10 -0300

    Pinos dos motores atualizados novamente: estranho

--- firmware_navigation/include/Config.h
+++ firmware_navigation/include/Config.h
@@ -24,8 +24,8 @@
 #define M1_KP           1.5
 #define M1_KI           0.75
 
-#define M3_ENA          GPIO_NUM_32           // ENA
-#define M3_ENB          GPIO_NUM_33           // ENB
+#define M3_ENA          GPIO_NUM_33           // ENA
+#define M3_ENB          GPIO_NUM_32           // ENB
 #define M3_PWM          GPIO_NUM_25
 #define M3_ENC_A        GPIO_NUM_35
 #define M3_ENC_B        GPIO_NUM_34         
@@ -45,8 +45,8 @@
 #define M4_ENA          GPIO_NUM_19
 #define M4_ENB          GPIO_NUM_18
 #define M4_PWM          GPIO_NUM_21
-#define M4_ENC_A        GPIO_NUM_39
-#define M4_ENC_B        GPIO_NUM_36
+#define M4_ENC_A        GPIO_NUM_36 //VN
+#define M4_ENC_B        GPIO_NUM_39 //VP
 #define M4_CANAL_PWM    LEDC_CHANNEL_3
 #define M4_KP           1.5
 #define M4_KI           0.75
